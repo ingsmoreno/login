1d8d54c1fce2d9026f46a165e890558667757fce
Saray Moreno <saraymoreno@192.168.1.15>
2025-01-05 17:55:50 -0300

    class validator , class transformer dependencies added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,8 @@ async function bootstrap() {
 
   SwaggerModule.setup('documentation', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
