0348490db62ff1b50cb69aed7270939cd889c951
Saray Moreno <saraymoreno@Laptop-de-Saray.local>
2025-01-07 18:38:48 -0300

    register and login validations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Users, UsersDocument } from './schema/users.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+// import { FindUserDto } from './dto/find-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
@@ -8,22 +9,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(Users.name) private usersModule: Model<UsersDocument>,
+    @InjectModel(Users.name) private usersModel: Model<UsersDocument>,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const userCreated = await this.usersModule.create(createUserDto);
+    const userCreated = await this.usersModel.create(createUserDto);
 
     return userCreated;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.usersModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  // async findOne(email: FindUserDto) {
+  //   // return await this.usersModule.findOne(email);
+  // }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   update(id: number, updateUserDto: UpdateUserDto) {
