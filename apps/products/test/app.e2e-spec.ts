c9f52bd94f5af4d045d86e67cffdf8ad2cf01635
Saray Moreno <saraymoreno@192.168.1.13>
2025-02-02 19:52:34 -0300

    testing files

--- apps/products/test/app.e2e-spec.ts
+++ apps/products/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+// import * as request from 'supertest';
 import { ProductsModule } from './../src/products.module';
 
 describe('ProductsController (e2e)', () => {
@@ -15,10 +15,10 @@ describe('ProductsController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
 });
